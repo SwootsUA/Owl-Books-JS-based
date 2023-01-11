d575821c03359d003067eb2c5ed81bf7437d6a20
Kostya <kostya20022210@gmail.com>
2023-01-11 21:18:21 +0200

    animated search for phones
    now search working for screens with width less then 540px

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 const menuIcon = document.querySelector('.burger-menu_button');
 const navigation = document.querySelector('.burger-menu_items');
-const menuItems = document.querySelector('.burger-menu-item-container')
+const menuItems = document.querySelector('.burger-menu-item-container');
 
 menuIcon.onclick = function () {
     menuIcon.classList.toggle('active');
@@ -13,3 +13,27 @@ navigation.onclick = function () {
     navigation.classList.remove('active');
     menuItems.classList.remove('active');
 }
+
+const searchIcon = document.querySelector('.search__img');
+const searchBorder = document.querySelector('.search__border');
+const searchCross = document.querySelector('.search__cross');
+const cartImg = document.querySelector('.cart__img');
+
+function revealCart() {
+    cartImg.classList.toggle('hidden');
+}
+
+searchIcon.onclick = function () {
+    searchBorder.classList.toggle('active');
+    searchIcon.classList.toggle('hidden');
+    searchCross.classList.toggle('hidden');
+    cartImg.classList.toggle('hidden');
+}
+
+searchCross.onclick = function () {
+    searchBorder.classList.toggle('active');
+    searchIcon.classList.toggle('hidden');
+    searchCross.classList.toggle('hidden');
+    setTimeout(revealCart, 350);
+}
+
