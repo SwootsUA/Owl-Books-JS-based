cde7904a5044032cece17683560978a3d4f16be3
Kostya <kostya20022210@gmail.com>
2023-01-11 10:13:06 +0200

    background transition instantaneously

--- js/app.js
+++ js/app.js
@@ -1,12 +1,15 @@
 const menuIcon = document.querySelector('.burger-menu_button');
 const navigation = document.querySelector('.burger-menu_items');
+const menuItems = document.querySelector('.burger-menu-item-container')
 
 menuIcon.onclick = function () {
     menuIcon.classList.toggle('active');
     navigation.classList.toggle('active');
+    menuItems.classList.toggle('active');
 }
 
 navigation.onclick = function () {
     menuIcon.classList.remove('active');
     navigation.classList.remove('active');
+    menuItems.classList.remove('active');
 }
