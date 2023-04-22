baa89dbf00babe17e9403ac091097992f899b4f8
Kostya <kostya20022210@gmail.com>
2023-04-22 14:57:36 +0300

    remove includes

--- pages/make-order.php
+++ pages/make-order.php
@@ -8,18 +8,45 @@
 <html lang="ua">
 	<head>
 		<title>Оформлення замовлення</title>
-        <?php include($_SERVER["DOCUMENT_ROOT"]."/include/head-info.html");?>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="../css/style.css">
+        <link rel="icon" href="../img/book.png">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Akaya+Telivigala" rel="st ylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Inter" rel="stylesheet">
         <meta name="keywords" content="Книжний магазин, книги, манга, журнали, аксесуари до книг, Owl Books">
 		<meta name="author" content="Захарченко Костянтин">
 	</head>
 
 	<body>
         <nav class="burger-menu_items">
-			<?php include($_SERVER["DOCUMENT_ROOT"]."/include/burger-menu.html");?>
+            <div class="burger-menu-item-container">
+                <a href="../index.php" class="burger-menu__content">Головна</a>
+                <a href="../pages/books.php" class="burger-menu__content">Книги</a>
+                <a href="../pages/magazines.php" class="burger-menu__content">Журнали</a>
+                <a href="../pages/mangas.php" class="burger-menu__content">Манга</a>
+                <a href="../pages/accessorys.php" class="burger-menu__content">Аксесуари</a>
+            </div>
 		</nav>
 
 		<header class="header">
-			<?php include($_SERVER["DOCUMENT_ROOT"]."/include/header.html");?>
+            <div class="burger-menu_button">
+                <img src="../img/menu.png" alt="menu">
+            </div>
+
+            <a href="../pages/books.php" class="header__content">Книги</a>
+            <a href="../pages/magazines.php" class="header__content">Журнали</a>
+
+            <a href="../index.php" class="header__logo">
+                <img src="../img/logo.png" alt="logo" height="80">
+                <div>Owl Books</div>
+            </a>
+
+            <a href="../pages/mangas.php" class="header__content">Манга</a>
+            <a href="../pages/accessorys.php" class="header__content">Аксесуари</a>
 		</header>
 		
         <div class="popup-order-succeed">
@@ -116,7 +143,18 @@
         </div>
 
 		<footer class="footer">
-			<?php include($_SERVER["DOCUMENT_ROOT"]."/include/footer.html");?>
+            <div class="footer__copyright">© 2023 “Owl Books”</div>
+            <div class="footer__about">
+                <a class="footer__about__social" href="https://telegram.org">
+                    <img class="footer__about__social_img" src="../img/teleg.png" alt="telegram">
+                </a>
+                <a class="footer__about__social" href="https://instagram.com">
+                    <img class="footer__about__social_img" src="../img/insta.png" alt="instagram">
+                </a>
+                <a class="footer__about__social" href="https://facebook.com">
+                    <img class="footer__about__social__img" src="../img/faceb.png" alt="facebook">
+                </a>
+            </div>
 		</footer>
 		
 		<script type="module" src="../js/app.js"></script>
